e9364dd17166fb3c66b6c4c882b9b659fc313085
Tey-Jo <taehee.jo.88@gmail.com>
2020-07-07 21:10:31 +0200

    draw and changing colors
    function for line drawing,
    function for changing color

--- app.js
+++ app.js
@@ -1,4 +1,16 @@
 const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
+const colors = document.getElementsByClassName("jsColor");
+
+
+canvas.width = 600;
+canvas.height = 600;
+// canvas need to have pixel modifier size which is real size in window, not only css size
+
+
+ctx.strokeStyle = "default";
+ctx.lineWidth = 2.5;
+
 
 let painting = false;
 
@@ -6,23 +18,41 @@ function stopPainting() {
   painting = false;
 }
 
+function startPainting() {
+  painting = true;
+}
+
 function onMouseMove(event) {
   const x = event.offsetX;
   const y = event.offsetY;
+  if (!painting) {
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+    // create path when mouse start to moving on canvas
+  } else {
+    ctx.lineTo(x, y);
+    ctx.stroke();
+    // create line on path when mouseevent(click) appear
+  }
 }
 
 function onmousedown(event) {
   painting = true;
 }
 
-function onmouseup(event) {
-  stopPainting();
+function handleColorClick(event) {
+  const color = event.target.style.backgroundColor;
+  ctx.strokeStyle = color;
 }
 
 
 if (canvas) {
   canvas.addEventListener("mousemove", onMouseMove);
-  canvas.addEventListener("mousedown", onmousedown);
-  canvas.addEventListener("mouseup", onmouseup);
+  canvas.addEventListener("mousedown", startPainting);
+  canvas.addEventListener("mouseup", stopPainting);
   canvas.addEventListener("mouseleave", stopPainting);
 }
+
+Array.from(colors).forEach(color => color.addEventListener("click", handleColorClick));
+// make array for each color , and pick up from array when event appear
+
